7b5d49c51229d2d2c6a0da552fac7292b06811bb
helloserve <helloserve@gmail.com>
2015-07-28 21:43:04 +0200

    new messaging component

---
+++ helloserve.com.Messaging/helloserve.com.Messaging/Config/Email.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloserve.com.Messaging.Config
+{
+    public class SmtpServer : ConfigurationElement
+    {
+        [ConfigurationProperty("address", IsRequired = false, DefaultValue = "localhost")]
+        public string Address
+        {
+            get { return (string)this["address"]; }
+            set { this["address"] = value; }
+        }
+
+        [ConfigurationProperty("port", IsRequired = false)]
+        public int? Port
+        {
+            get { return (int?)this["port"]; }
+            set { this["port"] = value; }
+        }
+
+        [ConfigurationProperty("username", IsRequired = false)]
+        public string Username
+        {
+            get { return (string)this["username"]; }
+            set { this["username"] = value; }
+        }
+
+        [ConfigurationProperty("password", IsRequired = false)]
+        public string Password
+        {
+            get { return (string)this["password"]; }
+            set { this["password"] = value; }
+        }
+
+        [ConfigurationProperty("ssl", IsRequired = false, DefaultValue = false)]
+        public bool Ssl
+        {
+            get { return (bool)this["ssl"]; }
+            set { this["ssl"] = value; }
+        }
+    }
+
+    public class SmtpServers : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new SmtpServer();
+        }
+
+        protected override ConfigurationElement CreateNewElement(string elementName)
+        {
+            return base.CreateNewElement(elementName);
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return (element as SmtpServer).Address;
+        }
+
+        public new SmtpServer this[string server]
+        {
+            get
+            {
+                return BaseGet(server) as SmtpServer;
+            }
+            set
+            {
+                int index = BaseIndexOf(value);
+                if (index > 0)
+                {
+                    BaseRemoveAt(index);
+                    BaseAdd(index, value);
+                }
+                else
+                    BaseAdd(value);
+            }
+        }
+    }
+
+    public class Email : ConfigurationSection
+    {
+        [ConfigurationProperty("servers", IsRequired = true)]
+        [ConfigurationCollection(typeof(SmtpServers))]
+        public SmtpServers Servers
+        {
+            get { return this["servers"] as SmtpServers; }
+            set { this["servers"] = value; }
+        }
+    }
+}
---
+++ helloserve.com.Messaging/helloserve.com.Messaging/Email.cs
@@ -0,0 +1,67 @@
+﻿using helloserve.com.Messaging.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloserve.com.Messaging
+{
+    public class Email : IEmail
+    {
+        private static Action<System.Net.Mail.MailMessage> _sendEmail;
+        public static void SetSendEmail(Action<System.Net.Mail.MailMessage> sendEmail)
+        {
+            _sendEmail = sendEmail;
+        }
+
+        private Config.SmtpServer _server;
+
+        public Email(string smtpServer, int? port = null, string smtpUsername = null, string smtpPassword = null, bool smtpSsl = false)
+        {
+            _server = new Config.SmtpServer()
+            {
+                Address = smtpServer,
+                Port = port,
+                Username = smtpUsername,
+                Password = smtpPassword,
+                Ssl = smtpSsl
+            };
+        }
+
+        public Email(string server, string configSection = "helloserve.com.Email")
+        {
+            Config.Email config = ConfigurationManager.GetSection(configSection) as Config.Email;
+            _server = config.Servers[server];
+        }
+
+        public void SendEmail(System.Net.Mail.MailMessage message)
+        {
+            using (System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient())
+            {
+                if (string.IsNullOrEmpty(_server.Address) && _sendEmail != null)
+                {
+                    _sendEmail(message);
+                };
+
+                if (_server.Address.ToLower() != "localhost")
+                {
+                    client.Host = _server.Address;
+                    if (_server.Port.HasValue)
+                        client.Port = _server.Port.Value;
+
+                    string username = _server.Username;
+                    if (!string.IsNullOrEmpty(username))
+                        client.Credentials = new System.Net.NetworkCredential(username, _server.Password);
+                }
+
+                client.EnableSsl = _server.Ssl;
+                client.Send(message);
+            }
+
+            message.Dispose();
+        }
+
+    }
+}
