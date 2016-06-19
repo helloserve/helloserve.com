36cf7ba3ae427b33d289d2067f7647f605db684c
helloserve <helloserve@gmail.com>
2016-06-19 15:44:33 +0200

    Ioc project.

---
+++ helloserve.com.Ioc/helloserve.com.Ioc/Ioc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloserve.com.Ioc
+{
+    public class Ioc : IocManager
+    {
+        private static Ioc _instance;
+
+        public static Ioc Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Ioc();
+
+                return _instance;
+            }
+        }        
+    }
+}
