bdb29e3cc18a82dbb9327636c986027f00c3d5c6
Henk Roux <henk@armorica.gk>
2016-07-18 09:58:45 +0200

    unit tests for Ioc manager, bug fixes, font update to website.

---
+++ helloserve.com.Ioc/helloserve.com.Ioc.Tests/IocManagerTests.cs
@@ -0,0 +1,63 @@
+﻿using helloserve.com.Ioc.Tests.Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloserve.com.Ioc.Tests
+{
+    [TestClass]
+    public class IocManagerTests
+    {
+        Manager _manager;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _manager = new Implementation.Manager();
+        }
+
+        [TestMethod]
+        public void InstantiateManager_Exists()
+        {
+            Assert.IsNotNull(_manager);
+        }
+
+        [TestMethod]
+        public void RegisterType_CanGetInstance()
+        {
+            _manager.RegisterTypeInstance<IInterfaceA>(() => new ConcreteA());
+            var instance = _manager.GetTypeInstance<IInterfaceA>();
+            Assert.IsNotNull(instance as IInterfaceA);
+            Assert.IsNotNull(instance as ConcreteA);
+        }
+
+        [TestMethod]
+        public void NotRegisterType_GetsException()
+        {
+            bool exception = false;
+            try
+            {
+                var instance = _manager.GetTypeInstance<IInterfaceA>();
+            }
+            catch
+            {
+                exception = true;
+            }
+
+            Assert.IsTrue(exception);
+        }
+
+        [TestMethod]
+        public void RegisterType_ThenSubstituteType_GetCorrectInstance()
+        {
+            _manager.RegisterTypeInstance<IInterfaceA>(() => new ConcreteA());
+            _manager.RegisterTypeInstance<IInterfaceA>(() => new ConcreteB());
+            var instance = _manager.GetTypeInstance<IInterfaceA>();
+            Assert.IsNotNull(instance as IInterfaceA);
+            Assert.IsNotNull(instance as ConcreteB);
+        }
+    }
+}
--- helloserve.com.Ioc/helloserve.com.Ioc/IocManager.cs
+++ helloserve.com.Ioc/helloserve.com.Ioc/IocManager.cs
@@ -10,12 +10,12 @@ namespace helloserve.com.Ioc
     {
         private Dictionary<string, Func<object>> _funcs = new Dictionary<string, Func<object>>();
 
-        public void SetTypeFunc<T>(Func<T> func)
+        public void RegisterTypeInstance<T>(Func<T> func)
         {
             Type type = typeof(T);
             string name = type.FullName;
 
-            if (_funcs.ContainsKey(name))
+            if (!_funcs.ContainsKey(name))
                 _funcs.Add(name, func as Func<object>);
             else
                 _funcs[name] = func as Func<object>;
@@ -29,7 +29,7 @@ namespace helloserve.com.Ioc
             if (_funcs.ContainsKey(name))
                 return (_funcs[name] as Func<T>)();
 
-            throw new KeyNotFoundException(string.Format("Type '{0}' not configured.", name));
+            throw new KeyNotFoundException(string.Format("Type '{0}' not registered.", name));
         }
     }
 }
