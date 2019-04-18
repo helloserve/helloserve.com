2efbf8c04967684df88a0d575a9a7a84b15a93f9
Henk Roux <helloserve@hotmail.com>
2019-04-18 07:29:54 +0200

    implement SyndicationHostedService

---
+++ src/helloserve.com.Domain/Syndication/SyndicationHostedService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace helloserve.com.Domain.Syndication
+{
+    public class SyndicationHostedService : BackgroundService
+    {
+        readonly IBlogSyndicationQueue _queue;
+        readonly ILogger _logger;
+
+        public SyndicationHostedService(IBlogSyndicationQueue queue, ILoggerFactory loggerFactory)
+        {
+            _queue = queue;
+            _logger = loggerFactory.CreateLogger<SyndicationHostedService>();
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            IBlogSyndication syndicationItem = _queue.Dequeue();
+        }
+    }
+}
---
+++ test/helloserve.com.Test.Domain/Syndication/SyndicationHostedServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using helloserve.com.Domain.Syndication;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace helloserve.com.Test.Domain.Syndication
+{
+    [TestClass]
+    public class SyndicationHostedServiceTests
+    {
+        [TestMethod]
+        public async Task ExecuteAsync_Verify()
+        {
+            //arrange
+            Mock<IBlogSyndicationQueue> queueMock = new Mock<IBlogSyndicationQueue>();
+            Mock<ILoggerFactory> loggerFactoryMock = new Mock<ILoggerFactory>();
+            SyndicationHostedService service = new SyndicationHostedService(queueMock.Object, loggerFactoryMock.Object);
+            CancellationToken cancellationToken = new CancellationToken();
+
+            //act
+            await service.StartAsync(cancellationToken);
+
+            //
+            queueMock.Verify(x => x.Dequeue());
+        }
+    }
+}
