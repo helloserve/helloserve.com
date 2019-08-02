c4906df8620aac55b00fa280d23d4037f87fcfbc
Henk Roux <helloserve@gmail.com>
2019-08-02 20:40:56 +0200

    add meta header component and PageState service

---
+++ src/helloserve.com/Services/PageState.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace helloserve.com
+{
+    public class PageState
+    {
+        public static PageState Current { get; internal set; }
+
+        static PageState()
+        {
+            Current = PageState.Default;
+        }
+
+        public static void Reset()
+        {
+            PageState prev = Current;
+            Current = PageState.Default;
+            Current.OnStateChange = prev.OnStateChange;
+            Current.StateChanged();
+        }
+
+        static PageState Default => new PageState()
+        {            
+            Title = "helloserve.com",
+            Description = string.Empty
+        };
+
+        private string title;
+        public string Title {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+                StateChanged();
+            }
+        }
+
+        private string description;
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                description = value;
+                StateChanged();
+            }
+        }
+
+        public event EventHandler OnStateChange;
+
+        private void StateChanged()
+        {
+            OnStateChange?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
