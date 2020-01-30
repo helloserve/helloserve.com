8a869e177cd10a33399dbd05ebe95355fecf1096
helloserve <helloserve@gmail.com>
2020-01-30 09:21:49 +0200

    small tweaks to blue car page and project detail

--- db/Migration_B_Projects.sql
+++ db/Migration_B_Projects.sql
@@ -1,22 +1,24 @@
-DECLARE @Projects TABLE ([Key] NVARCHAR(150), Name NVARCHAR(250), ComponentPage NVARCHAR(250), IsActive BIT, SortOrder INT)
+DECLARE @Projects TABLE ([Key] NVARCHAR(150), Name NVARCHAR(250), ComponentPage NVARCHAR(250), IsActive BIT, SortOrder INT, Description NVARCHAR(MAX), ImageUrl NVARCHAR(500))
 
-INSERT INTO @Projects ([Key], Name, ComponentPage, IsActive, SortOrder)
-VALUES ('stingrayincursion', 'Stingray Incursion', 'StingrayIncursion', 1, 3),
-	   ('thebluecar', 'The Blue Car', 'TheBlueCar', 1, 1),
-	   ('thesarienexperiment', 'The Sarien Experiment', 'SarienExperiment', 1, 2)
+INSERT INTO @Projects ([Key], Name, ComponentPage, IsActive, SortOrder, Description, ImageUrl)
+VALUES ('stingrayincursion', 'Stingray Incursion', 'StingrayIncursion', 1, 3, NULl, NULL),
+	   ('thebluecar', 'The Blue Car', 'TheBlueCar', 1, 1, 'Affectionally referred to by its colour, this 1989 Eunos Roadster is my first project car, and has a significant bond with our family.', 'media/20180609_162848.jpg'),
+	   ('thesarienexperiment', 'The Sarien Experiment', 'SarienExperiment', 1, 2, NULL, NULL)
 
 MERGE Projects AS target
-USINg (SELECT [Key], Name, ComponentPage, IsActive, SortOrder FROM @Projects) AS source
+USINg (SELECT [Key], Name, ComponentPage, IsActive, SortOrder, Description, ImageUrl FROM @Projects) AS source
 ON target.[Key] = source.[Key]
 WHEN MATCHED THEN
 	UPDATE
 	SET Name = source.Name,
 		ComponentPage = source.ComponentPage,
 		IsActive = source.IsActive,
-		SortOrder = source.SortOrder
+		SortOrder = source.SortOrder,
+		Description = source.Description,
+		ImageUrl = source.ImageUrl
 WHEN NOT MATCHED THEN
-	INSERT ([Key], Name, ComponentPage, IsActive, SortOrder)
-	VALUES (source.[Key], source.Name, source.ComponentPage, source.IsActive, source.SortOrder);
+	INSERT ([Key], Name, ComponentPage, IsActive, SortOrder, Description, ImageUrl)
+	VALUES (source.[Key], source.Name, source.ComponentPage, source.IsActive, source.SortOrder, source.Description, source.ImageUrl);
 
 
 IF EXISTS (SELECT * FROM sys.tables t JOIN sys.columns c ON t.object_id = c.object_id AND t.name = 'BlogOwners' AND c.name = 'OwnerId')
